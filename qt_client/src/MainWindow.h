2cc619491157a54e05e503d1cf55642c5c0e99be
ForkMesh Deploy <deploy@forkmesh.local>
2026-08-02 06:13:24 -0400

    fix(qt): guard MainWindow header for PCH

--- qt_client/src/MainWindow.h
+++ qt_client/src/MainWindow.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef FORKMESH_MAIN_WINDOW_H
+#define FORKMESH_MAIN_WINDOW_H
 
 #include "ChatBackend.h"
 #include "DiscussionInboxBackoff.h"
@@ -8177,3 +8178,5 @@ private:
     TestIssueHistoryDeleteRunner m_testIssueHistoryDeleteRunner;
 #endif
 };
+
+#endif // FORKMESH_MAIN_WINDOW_H
